c129cd8c2a89db228f79f9a5ed7fe241699cc3bc
Headpenguin <headpenguin.penguinarts@gmail.com>
2022-07-07 14:26:20 -0400

    Added code to register this client with the udp server and to retrieve proximity parameters

---
+++ Packets.py
@@ -0,0 +1,99 @@
+from struct import *
+from enum import *
+import uuid
+
+HEADER_FORMAT = '<2h'
+INIT_FORMAT = '<2f'
+
+HEADER_LEN = 20
+INIT_LEN = calcsize(INIT_FORMAT)
+
+class ZeroIndex(IntEnum):
+    def _generate_next_value_(name, start, count, last_values):
+         return count
+
+class PacketTypes(ZeroIndex):
+    Unknown = auto()
+    Init = auto()
+    Player = auto()
+    Cap = auto()
+    Game = auto()
+    Tag = auto()
+    Connect = auto()
+    Disconnect = auto()
+    Costume = auto()
+    Shine = auto()
+    Capture = auto()
+    ChangeStage = auto()
+    ChatConnect = auto()
+    ChatInit = auto()
+    ChatVoice = auto()
+    Command = auto()
+
+
+
+class Header:
+    def __init__(self, GUID, ty, size):
+        self.GUID, self.ty, self.size = (GUID, ty, size)
+        
+    @classmethod
+    def unpack(cls, msg):
+        ty, size = unpack(HEADER_FORMAT, msg[16:])
+        GUID = uuid.UUID(bytes_le=msg[:16])
+        return cls(GUID, ty, size)
+
+    def pack(self):
+        return  self.GUID.bytes_le + pack(HEADER_FORMAT, self.ty, self.size)
+
+class Packet:
+    ClientGUID = uuid.UUID(bytes=b'\x00'*16)
+    def __init__(self, header):
+        self.header = header
+
+    @classmethod
+    def unpack(cls, header, msg):
+        return cls(header)
+
+    def pack(self):
+        return self.header.pack()
+
+class Connect(Packet):
+    def __init__(self, name):
+        Packet.__init__(self, Header(Packet.ClientGUID, PacketTypes.ChatConnect, len(name.encode())))
+        self.name = name
+        
+    @classmethod
+    def unpack(cls, header, msg):
+        obj = cls(msg.decode())
+        obj.header = header
+        return obj
+
+    def pack(self):
+        return (Packet.pack(self), self.name.encode())
+
+class Init(Packet):
+    def __init__(self, silenceDistance, peakDistance):
+        Packet.__init__(self, Header(Packet.ClientGUID, PacketTypes.ChatInit, 8))
+        self.silenceDistance = silenceDistance
+        self.peakDistance = peakDistance
+
+    @classmethod
+    def unpack(cls, header, msg):
+        silenceDistance, peakDistance = unpack(INIT_FORMAT, msg)
+        obj = cls(silenceDistance, peakDistance)
+        obj.header = header
+        return obj
+
+    def pack(self):
+        return(Packet.pack(self), pack(INIT_FORMAT, self.silenceDistance, self.peakDistance))
+
+    def setGUID(self):
+        Packet.ClientGUID = self.header.GUID
+
+PACKET_TYPE_TABLE = {
+    PacketTypes.Unknown: Packet,
+    PacketTypes.ChatConnect: Connect,
+    PacketTypes.ChatInit: Init
+}        
+        
+        
--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 
 ''' |
     v''' 
-NAME = "Test" # Modify this to change your in-game name
+NAME = "Sussy 1" # Modify this to change your in-game name
+IP = "127.0.0.1" # Modify this to set the server ip address
+PORT = None # Modify this to set the server port number
 '''^
    |'''
 
@@ -11,12 +13,35 @@ NAME = "Test" # Modify this to change your in-game name
 import os
 import sys
 import time
+import socket
+
 import numpy as np
 import sounddevice as sd
 
+import Packets
+
 TIME_TRAVEL = 25 # How long we wait to play back audio we recieve, in milliseconds
-SILENCE_DISTANCE = 10 # The farthest a player can be and still be heard
-PEAK_DISTANCE = 3 # How close a player must be to be heard at the maximum volume
+SILENCE_DISTANCE = 10 # The farthest a player can be and still be heard (this is the default, the actual value is loaded from the server)
+PEAK_DISTANCE = 3 # How close a player must be to be heard at the maximum volume (this is the default, the actual value is loaded from the server)
+
+def send(connection, data, address):
+    total = connection.sendto(data[0], address)
+    total += connection.sendto(data[1], address)
+    if total < len(data[0]) + len(data[1]):
+        return False
+    return True
+
+def receive(connection, expectedAddress):
+    data, address = (None, None)
+    while address != expectedAddress:
+        data, address = connection.recvfrom(Packets.HEADER_LEN)
+    header = Packets.Header.unpack(data)
+    data, address = (None, None)
+    while address != expectedAddress:
+        data, address = connection.recvfrom(header.size)
+    packet = Packets.PACKET_TYPE_TABLE[header.ty].unpack(header, data)
+    return packet
+    
 
 if NAME != None:
     pass
@@ -26,7 +51,7 @@ elif "IN_GAME_NAME" in os.environ.keys():
     NAME = os.environ["IN_GAME_NAME"]
 else:
     print("No name has been provided, so proximity chat cannot function. Please provide a name in one of the following ways:", file=sys.stderr)
-    print("    1. Provide your name as an argument to this Python script. For example, enter `python3 proximity-chat.py myUser_nameIs-reallyc0ol` into your command prompt.", file=sys.stderr)
+    print("    1. Provide your name as the first argument to this Python script. For example, enter `python3 proximity-chat.py myUser_nameIs-reallyc0ol ipAddress` into your command prompt.", file=sys.stderr)
     print("    2. Put you name into the environment variable IN_GAME_NAME.", file=sys.stderr)
     print("    3. Enter your name directly into this script. To do so, open this file (proximity-chat.py), and to the right of the `=` sign next to the variable called `NAME` near the top of the file, type your desired name and surround it \
 with quotes. For example, change the line `NAME = None` to `NAME = 'MyUserNameIsReallyEpic'`.", file=sys.stderr)
@@ -34,9 +59,63 @@ with quotes. For example, change the line `NAME = None` to `NAME = 'MyUserNameIs
     print("Now exiting")
     sys.exit(-1)
 
-t = np.linspace(0, 3, 3 * 44100, False)
+if IP != None:
+    pass
+elif "SMO_HOST_SERVER_IP" in os.environ.keys():
+    IP = os.environ["SMO_HOST_SERVER_IP"]
+elif len(sys.argv) > 1:
+    IP = sys.argv[-1]
+else:
+    print("No ip has been provided, so proximity chat cannot function. Please provide an ip in one of the following ways:", file=sys.stderr)
+    print("    1. Provide your host server's ip as the final argument to this Python script. For example, enter `python3 proximity-chat.py myUser_nameIs-reallyc0ol ipAddress` into your command prompt.", file=sys.stderr)
+    print("    2. Put you ip into the environment variable SMO_HOST_SERVER_IP.", file=sys.stderr)
+    print("    3. Enter your name directly into this script. To do so, open this file (proximity-chat.py), and to the right of the `=` sign next to the variable called `IP` near the top of the file, type your host server's ip and surround it \
+with quotes. For example, change the line `IP = None` to `IP = '123.45.67.89'`.", file=sys.stderr)
+    print("Make sure that the ip you enter matches your host server's ip, or else this program will not be able to connect to the server", file=sys.stderr)
+    print("Now exiting")
+    sys.exit(-1)
 
-print("%s, %s, %s, %s, %s, %s, %s, %s" %(t[0], t[1], t[2], t[3], t[4], t[5], t[6], t[7]))
+if PORT != None:
+    pass
+elif "SMO_HOST_SERVER_PORT" in os.environ.keys():
+    PORT = int(os.environ["SMO_HOST_SERVER_PORT"])
+elif len(sys.argv) > 1:
+    try:
+        PORT = int(sys.argv[-1])
+    except ValueError:
+        if len(sys.argv) > 2:
+            try:
+                PORT = int(sys.argv[-2])
+            except ValueError:
+                pass
+else:
+    PORT = 48984
+    print("No port has been provided, so proximity chat cannot function. Please provide a port in one of the following ways:", file=sys.stderr)
+    print("    1. Provide your host server's port as the second-to-last argument to this Python script. For example, enter `python3 proximity-chat.py myUser_nameIs-reallyc0ol portNumber ipAddress` into your command prompt.", file=sys.stderr)
+    print("    2. Put your port into the environment variable SMO_HOST_SERVER_PORT.", file=sys.stderr)
+    print("    3. Enter your port directly into this script. To do so, open this file (proximity-chat.py), and to the right of the `=` sign next to the variable called `PORT` near the top of the file, type your host server's port number. \
+For example, change the line `PORT = None` to `PORT = 12345'`.", file=sys.stderr)
+    print("Make sure that the port you enter matches your host server's port, or else this program will not be able to connect to the server", file=sys.stderr)
+    print("Resuming execution, defaulting port to 48984", file=sys.stderr)
+          
+connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+address = (IP, PORT)
+
+first = Packets.Connect(NAME)
+
+while not send(connection, first.pack(), address):
+    pass
+
+response = receive(connection, address)
+
+response.setGUID()
+
+SILENCE_DISTANCE = response.silenceDistance
+PEAK_DISTANCE = response.peakDistance
+
+#header = Packets.Packet(connection.recv(Packets.HEADER_LEN))
+
+t = np.linspace(0, 3, 3 * 44100, False)
 
 audio1 = np.sin(440 * t * 2 * np.pi)
 
@@ -47,5 +126,3 @@ sd.play(audio1, 44100)
 time.sleep(1)
 
 sd.play(audio2, 44100)
-
-print(NAME)
